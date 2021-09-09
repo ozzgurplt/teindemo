e95308f96c4e9373cc75f04b0d7022d88af491ca
ugur ciftci <ugurprog@gmail.com>
2021-09-10 01:03:52 +0300

    ..

--- src/main/java/com/tein/cmsproject/controller/EmployeeController.java
+++ src/main/java/com/tein/cmsproject/controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.tein.cmsproject.controller;
 import com.tein.cmsproject.model.Employee;
 import com.tein.cmsproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class EmployeeController {
@@ -15,40 +21,43 @@ public class EmployeeController {
 
 
 
-    @GetMapping("/")
+    @GetMapping("/admin/")
     public String viewHomePage(Model model){
         model.addAttribute("listEmployee",employeeService.getAllEmployees());
         return "index";
     }
 
-
-
-    @GetMapping("/showNewEmployeeForm")
+    @GetMapping("/user/showNewEmployeeForm")
     public String showNewEmployeeForm(Model model){
         Employee employee = new Employee();
         model.addAttribute("employee",employee);
         return "new_employee";
     }
 
-    @PostMapping("/saveEmployee")
+    @PostMapping("/user/saveEmployee")
     public String saveEmployee(@ModelAttribute("employee")Employee employee){
         employeeService.saveEmployee(employee);
-        return "redirect:/";
+        //TODO : kullanıcı sisteme kayıt edildi admin goruntuluyebilir
+        return "redirect:/admin/";
     }
-    @GetMapping("/showFormForUpdate/{id}")
+    @GetMapping("/user/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable( value = "id") long id, Model model){
         Employee employee = employeeService.getEmployeeById(id);
         model.addAttribute("employee", employee);
         return "update_employee";
     }
 
-    @GetMapping("/deleteEmployee/{id}")
+    @GetMapping("/user/deleteEmployee/{id}")
     public String deleteEmployee(@PathVariable (value = "id") long id) {
 
         // call delete employee method
         this.employeeService.deleteEmployeeById(id);
-        return "redirect:/";
+        return "redirect:/admin/";
     }
 
+    @GetMapping("/not-auth")
+    public String getAccessDenied() {
+        return "/not-auth";
+    }
 
 }
